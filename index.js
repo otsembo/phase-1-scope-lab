8918848de4526ed711d5acd07f9e91bfae3ef2bb
otsembo <okumu.otsembo@gmail.com>
2022-03-01 14:37:53 +0300

    commit tree 38f06767de6a15d9ef9068d13b1239d6f558208f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFav = 'someone';
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function  overwriteBestCustomer(best_customer){
+  bestCustomer = best_customer;
+}
+
+function  changeLeastFavoriteCustomer(){
+  leastFav = 'bob';
+}
